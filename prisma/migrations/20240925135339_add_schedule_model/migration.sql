1ce6731c26ffdeec97b8e0db176c45c18ff0ce82
ivanvilashek <ivan.vilashek@gmail.com>
2024-09-26 00:08:05 +0300

    chore: add schedule model

---
+++ prisma/migrations/20240925135339_add_schedule_model/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Schedule" (
+    "id" TEXT NOT NULL,
+    "train" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "arrival" TEXT NOT NULL,
+    "departure" TEXT NOT NULL,
+    "departureAt" TIMESTAMP(3) NOT NULL,
+    "arrivalAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Schedule_pkey" PRIMARY KEY ("id")
+);
